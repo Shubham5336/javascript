f8d0e8f8a5223408ec40a1d30fe3704d05227350
Shubham5336 <148366238+Shubham5336@users.noreply.github.com>
2024-07-08 19:52:08 +0000

    container config done

---
+++ basics/variable.js
@@ -0,0 +1,11 @@
+const accountId="1234"
+let accountEmail="shubham.gamil.com"
+var accountPassword="2345"
+accountState="up"
+accountEmail="shubham.yahoo.com"
+accountPassword="9971"
+accountState="delhi"
+/* var ko use nhi karna let use karna hai
+var me block scope aur functional scope ki dikkat aati hai
+var phele use hota tha */
+console.table([accountEmail,accountId, accountPassword, accountState])
