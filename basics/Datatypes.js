ac030ad2c2cbbc82d2debe3e64586e2a1d4f9cca
Shubham5336 <148366238+Shubham5336@users.noreply.github.com>
2024-07-09 19:33:10 +0000

    container config done

---
+++ basics/Datatypes.js
@@ -0,0 +1,8 @@
+let name="shubham" // string
+let  age=20 // number  range is 2 power 53
+let Email= null // null is alsp a data type 
+address; // it will give a undefined value
+ let input = true/false // boolean 
+
+ console.log(typeof null); // the output of null is a object
+ console.log(typeof undefined); // the output of undefined  is also undefined.
---
+++ basics/datatypeConversion.js
@@ -0,0 +1,23 @@
+let value= undefined
+let score = String(value)
+console.log(typeof score);
+console.log(score);
+/*
+in the type conversion of number to other data types
+i/p value   o/p value                
+true         1
+false        0 
+"33"        33
+""           0
+"33abc"     NaN(not a number)
+null         0
+undefined    Nan
+*/
+
+/* 
+in the type conversion of string to other data types
+i/p value   o/p value  
+33          33 (it will take it as a string)
+null        null
+undefined   undefined
+*/
