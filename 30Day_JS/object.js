3ce2ba9b3695382a5371836b653ed289fd1d4c19
Shubham5336 <148366238+Shubham5336@users.noreply.github.com>
2024-07-19 18:20:39 +0000

    container config done

---
+++ 30Day_JS/object.js
@@ -0,0 +1,47 @@
+/*Task-1 
+const book={
+    Title:"book one",
+    Author:"robin",
+    Year:2000,
+    feedback:(()=>{
+        return `${book.Author}  ${book.Title}`
+    }),
+    year:((year)=>{
+        return `${book.Year=year}`
+    }),
+    this:(()=>{
+        return book.this(Title)
+        return book.this(Author)
+    })
+    
+}
+/*console.log(book);
+Task-2
+console.log(`The author of the book is ${book.Author} and Title pf the book is ${book.Title}`);
+Task-3
+console.log(book.feedback())
+Task-4
+console.log(book.year(2001));
+console.log(book);
+Task-5
+const library={
+    name:{
+        Title:["book1, book2, book3"],
+        publishedYear:2000
+    }
+}
+//console.log(library);
+Task-6
+console.log(library.name.Title);
+Task-7
+console.log(book.this(book.Author));
+Task-8
+for (const item in book) {
+    console.log(book[item]);
+}
+Task-9
+for (const item in book) {
+    console.log(Object.keys(book));
+    console.log(Object.values(book));
+}
+*/
